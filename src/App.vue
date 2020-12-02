ded2259f017255713cf6ad1a7449ba2f47c6306c
Jonathan Andersson <ja1984@gmail.com>
2020-12-02 09:30:48 +0100

    Fix bug in package

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
 
     <button @click="confirm('critical')">Critical</button>
     <button @click="confirm('information')">Information</button>
